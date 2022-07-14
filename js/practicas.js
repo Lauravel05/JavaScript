f143520721b1e4f4d90723895b3f86919dd667b7
Laura Velandia <lauravelandia2003@gmail.com>
2022-07-14 13:06:36 -0500

    ejempos cuadros de diálogo

---
+++ js/practicas.js
@@ -0,0 +1,37 @@
+let nombre = "Coder"
+let apellido = "House"
+let fechaNacimiento = "21/03/1975"
+let timbre
+const nombreCompleto = "laura velandia"
+
+// TIPADO
+// string= cadena de texto alfanumérica
+// Number= Numeros enteros o con decimales
+// Boolean= True - false
+
+
+
+let numeroA = 21
+let numeroB = 3
+
+let resultado = numeroA / numeroB
+let empresa = nombre + " " + apellido
+
+
+/* 
+
+CUADROS DE DIÁLOGO
+
+alert("") para mostrar mensajes de alerta al usuario 
+confirm("") para pedir confirmacion al suario
+prompt("") preguntarle al usuario un dato
+
+CONSOLE
+.log(mostrar en la consola)
+.warm(texto de advertencia)
+.error(mostrar error)
+*/
+
+let datoIngresado = prompt("Ingrese numero de su cuenta bancaria")
+console.log("NUMERO DE CUENTA:", datoIngresado)
+
