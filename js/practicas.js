ac41f3b8c5542a9972a2c52d0a5cf042584ca319
Laura Velandia <lauravelandia2003@gmail.com>
2022-07-21 09:14:20 -0500

    pruebas ciclos

--- js/practicas.js
+++ js/practicas.js
@@ -1,15 +1,41 @@
+/* 
+
+TIPADO
+
+string= cadena de texto alfanumérica
+Number= Numeros enteros o con decimales
+Boolean= True - false
+
+
+
+
+CUADROS DE DIÁLOGO
+
+alert("") para mostrar mensajes de alerta al usuario 
+confirm("") para pedir confirmacion al usario sobre una acció ACEPTAR o CANCELAR
+prompt("") preguntarle al usuario un dato en la pantalla
+
+
+
+
+CONSOLE
+
+.log(mostrar en la consola)
+.warm(signo de advertencia MENSAJE EN AMARILLO)
+.error(mostrar signo error MENSAJE EN ROJO)
+
+
+
+
+DECLARAR VARIABLE 
+
+let miVariable
+let miVarible2 = "Algo"
+
 let nombre = "Coder"
 let apellido = "House"
 let fechaNacimiento = "21/03/1975"
-let timbre
-const nombreCompleto = "laura velandia"
-
-// TIPADO
-// string= cadena de texto alfanumérica
-// Number= Numeros enteros o con decimales
-// Boolean= True - false
-
-
+const nombreCompleto = "laura velandia
 
 let numeroA = 21
 let numeroB = 3
@@ -18,21 +44,94 @@ let resultado = numeroA / numeroB
 let empresa = nombre + " " + apellido
 
 
-/* 
-
-CUADROS DE DIÁLOGO
-
-alert("") para mostrar mensajes de alerta al usuario 
-confirm("") para pedir confirmacion al suario
-prompt("") preguntarle al usuario un dato
-
-CONSOLE
-.log(mostrar en la consola)
-.warm(texto de advertencia)
-.error(mostrar error)
-*/
-
 let datoIngresado = prompt("Ingrese numero de su cuenta bancaria")
 console.log("NUMERO DE CUENTA:", datoIngresado)
+*/
 
-// hola
+
+/*
+let username = "coderhouse";
+
+if (username == "coderhouse") {
+    console.log(Bienvenido,username)
+}
+
+debugger
+
+if (username == "laura") {
+    console.warm(Bienvenido,username)
+}
+*/
+
+/*
+
+SEENTENCIA IF - ELSE 
+
+let username = prompt("Ingrese suuombre")
+
+debugger 
+
+if (username == "CoderHouse") {
+    console.log(Bienvenido,username)
+} else {
+    console.warn("No se reconoce el usuario")
+}
+
+
+
+let miOferta = prompt("Ingrese su oferta:")
+
+if (miOferta < 22){
+    console.error("Su oferta es demasiado baja")
+}
+
+else if (miOferta < 30){
+    console.error("Sigue muy baja, esturate un poco")
+}
+
+else if (miOferta >= 80){
+    console.log("Trato hecho")
+}
+
+*/
+
+/* IF - ELSE Y OPERADOR LOGICO AND 
+
+let username = prompt ("Ingres su nombre:")
+let password = prompt ("Ingrese contraseña")
+
+if (username == CoderHouse && password == 123456) {
+    console.log ("Bienvenido, username")
+} else {
+    console.warn("No se reconoce el usuario y/ la contraseña");
+}
+
+*/
+
+
+/*
+
+- CICLOS DE ITERACIÓN ESTÁTICO
+
+i++
+i += 1
+i = 1 + 1
+
+for (let i = 0; i < 21; i++){
+    console.log("Iteracion Nro.", i)
+}
+
+- CICLOS DE ITERACIÓN DINÁMICO
+
+let limite = parseInt(prompt("Ingresa un número:"))
+
+for (let i = 0; i < limite; i++){
+    console.log("Iteracion Nro.", i)
+}
+*/
+
+debugger 
+
+
+
+
